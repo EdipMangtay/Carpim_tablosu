a04ae3916f94fe7aa375546d96f633a85d0aa055
aeman <aeman@MANGTAY>
2023-06-26 19:33:53 +0300

    Add project files.

---
+++ Carpım_tablosu/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpım_tablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carpım();
+            Console.ReadLine();
+
+        }
+        public static void Carpım()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    int sonuc = i * j;
+                    Console.Write("{0} \t", sonuc);
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
